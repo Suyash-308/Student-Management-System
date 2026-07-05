b7c6d3e6fb206c4ba73caa818882ae30c5b489ef
Suyashnichit308 <nichitsuyash@gmail.com>
2026-07-05 11:03:27 +0530

    complete all crud operation

--- src/main/java/com/suyash/jwtauthapplication/controller/StudentController.java
+++ src/main/java/com/suyash/jwtauthapplication/controller/StudentController.java
@@ -3,10 +3,9 @@ package com.suyash.jwtauthapplication.controller;
 import com.suyash.jwtauthapplication.entity.Student;
 import com.suyash.jwtauthapplication.service.StudentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,8 +13,29 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudentController {
     private final StudentService studentService;
 
-    @PostMapping
-    public Student saveStudent(@RequestBody Student student) {
-        return studentService.saveStudent(student);
+    @PostMapping("/add")
+    public Student addStudent(@RequestBody Student student) {
+        return studentService.addStudent(student);
     }
+
+    @GetMapping("/{id}")
+    public Student getStudentById(@PathVariable int id) {
+        return studentService.getStudentById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Student updateStudent(@RequestBody Student student ,@PathVariable long id){
+        return studentService.updateStudent(id,student);
+    }
+
+    @GetMapping
+    public List<Student> getAllStudent(){
+        return studentService.getAllStudent();
+    }
+
+    @DeleteMapping
+    public void deleteAllStudent(){
+        studentService.deleteAllStudent();
+    }
+
 }
--- src/main/java/com/suyash/jwtauthapplication/service/StudentService.java
+++ src/main/java/com/suyash/jwtauthapplication/service/StudentService.java
@@ -4,6 +4,11 @@ import com.suyash.jwtauthapplication.entity.Student;
 import com.suyash.jwtauthapplication.repository.StudentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -11,7 +16,34 @@ public class StudentService {
 
     private final StudentRepository studentRepository;
 
-    public Student saveStudent(Student student) {
+    public Student addStudent(Student student) {
         return studentRepository.save(student);
+    }
+
+
+    public Student getStudentById(@PathVariable  long id) {
+        return studentRepository.findById(id).orElseThrow(() -> new RuntimeException("Student not found with id: " + id));
+    }
+
+    public Student updateStudent(long id, Student student) {
+        Student student1=studentRepository.findById(id).orElseThrow(()->new RuntimeException("student not found with id"+id));
+
+        student1.setName(student.getName());
+        student1.setEmail(student.getEmail());
+        student1.setCourse(student.getCourse());
+
+        return  studentRepository.save(student1);
+    }
+
+    public List<Student> getAllStudent() {
+        return studentRepository.findAll();
+    }
+
+    public void deleteById(long id) {
+        studentRepository.deleteById(id);
+    }
+
+    public void deleteAllStudent() {
+        studentRepository.deleteAll();
     }
 }
